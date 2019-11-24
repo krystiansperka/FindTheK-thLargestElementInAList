20af91973cd9cc83daad1dd72da33ec78466215f
Krystian Sperka <krystian.sperka@gmail.com>
2019-11-24 20:53:25 +0100

    Update Program.cs

--- FindTheK-thLargestElementInAList/Program.cs
+++ FindTheK-thLargestElementInAList/Program.cs
@@ -18,25 +18,44 @@ namespace FindTheK_thLargestElementInAList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            var solution = new Solution();
+            solution.RunTest1();
         }
     }
 
     public class Solution
     {
-        public int[] RunTest1()
+        public int RunTest1()
         {
-            int[] testArray = { 3, 5, 2, 4, 6, 8};
+            int[] testArray = { 3, 5, 2, 4, 6, 8 };
             int k = 3;
             return findKthLargest(testArray, k);
         }
 
-        public int[] findKthLargest(int[] inputArray, int kth)
+        public int findKthLargest(int[] inputArray, int kth)
         {
-            int[] sortedArray = new int[kth];
+            int temp;
 
-            for (int i = 0; i < inputArray.Length; i++)
+            for (int i = 0; i <= inputArray.Length - 2; i++)
             {
+                for (int j = 0; j <= inputArray.Length - 2; j++)
+                {
+                    if (inputArray[j] > inputArray[j + 1])
+                    {
+                        temp = inputArray[j + 1];
+                        inputArray[j + 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+
+                }
+                
 
             }
+
+            return inputArray[inputArray.Length-kth];
+        }
     }
+}
+        
